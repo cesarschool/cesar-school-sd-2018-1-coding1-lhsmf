f5bb6c33bd4b599b260b2147957c0c243846952c
lhsmf <38105337+lhsmf@users.noreply.github.com>
2018-06-18 19:06:45 -0300

    Update fizzbizz.c

--- Q1/fizzbizz.c
+++ Q1/fizzbizz.c
@@ -11,6 +11,19 @@ void fizzBizz(int start, int end)
 {
     printf("\nFizzBizz::START: %d, %d\n", start, end);
     
+   while (start < end){ 
+    if ((start % 3 == 0) && (start % 5 == 0)){
+      printf("FizzBuzz\n");
+    } else if (start % 3 == 0){
+      printf("Fizz\n");
+    }else if (start % 5 == 0){
+      printf("Buzz\n");
+    } else {
+      printf("%d\n", start);
+    }
+   start = start + 1;
+  }
+    
     // TODO: Your code here!
     // Ex.: printf("Fizz\n");
     
